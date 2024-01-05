b93261665db21ee62b1c5f4961eebd5f76331e80
hakantetik44 <fildisili88@gmail.com>
2024-01-05 15:02:53 +0100

    first commit

--- locators/Locators.py
+++
@@ -1,8 +0,0 @@
-
-class InsLocators:
-    AccepterCookies="xpath=//button[text()='Autoriser tous les cookies']"
-    btnFaceBaglan="xpath=//span[@class='_ab37']"
-    placeUsername="css=input[name='username']"
-    placePassword="css=input[name='password']"
-    btnGiris="xpath=//button[@type='submit']"
-    placaHataMesaji="css=._ab2z"
---
+++ locators/LoginLocators.py
@@ -0,0 +1,3 @@
+
+class LoginLocators:
+    seConnecterAvecGoogle="css=.nsm7Bb-HzV7m-LgbsSe-BPrWId"
